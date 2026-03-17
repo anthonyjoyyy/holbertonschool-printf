76bd0acf6315d455bb1fde2b9d2f2af62352ff60
Sebastion Price <sebastionprice@gmail.com>
2026-03-17 13:55:00 +1100

    Move finit to it's own file for clarity

--- _printf.c
+++ _printf.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * finit - initalize a variable of flag type
- * @f: variable to initialize
- */
-void finit(flag *f)
-{
-	if (f == NULL)
-		return;
-	f->zero = 0;
-	f->plus = 0;
-	f->minus = 0;
-	f->space = 0;
-	f->width = 0;
-	f->per = 0;
-}
-
-/**
  * _printf - printf clone
  *
  */
---
+++ finit.c
@@ -0,0 +1,17 @@
+#include "main.c"
+
+/**
+ * finit - initalize a variable of flag type
+ * @f: variable to initialize
+ */
+void finit(flag *f)
+{
+	if (f == NULL)
+		return;
+	f->zero = 0;
+	f->plus = 0;
+	f->minus = 0;
+	f->space = 0;
+	f->width = 0;
+	f->per = 0;
+}
