c7849aed63d330e862c6a4d292568d68ddfcfc39
Sebastion Price <sebastionprice@gmail.com>
2026-03-17 13:20:32 +1100

    Add tester for finit

---
+++ tests/finit-tester.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "../main.h"
+
+int main()
+{
+	format fro;
+	finit(&fro);
+	printf("zero: %d\n", fro.zero);
+	printf("plus: %d\n", fro.plus);
+	printf("minus: %d\n", fro.minus);
+	printf("space: %d\n", fro.space);
+	printf("width: %d\n", fro.width);
+	printf("per: %d\n", fro.per);
+	return 0;
+}
