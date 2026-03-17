d32e1f0135e45722eaf894e676a679cd3ad5deac
Sebastion Price <sebastionprice@gmail.com>
2026-03-17 13:46:36 +1100

    Add _printf function - printing single string

--- _printf.c
+++ _printf.c
@@ -4,9 +4,9 @@
  * finit - initalize a variable of flag type
  * @f: variable to initialize
  */
- void finit(flag *f)
- {
- 	if (f == NULL)
+void finit(flag *f)
+{
+	if (f == NULL)
 		return;
 	f->zero = 0;
 	f->plus = 0;
@@ -20,4 +20,24 @@
  * _printf - printf clone
  *
  */
- int _printf(const char *format, ...);
+int _printf(const char *format, ...)
+{
+	char const *a;
+	int x = 0;
+	int i = 0;
+
+	while (format[x])
+	{
+		if (format[x] == '%')
+		{
+			/**code*/
+		}
+		else
+		{
+			a = &format[x];
+			write(1, a, 1);
+			x++;
+		}
+	}
+	return i;
+}
---
+++ tests/base-test.c
@@ -0,0 +1,8 @@
+#include "../main.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+		_printf(argv[1]);
+	return (0);
+}
