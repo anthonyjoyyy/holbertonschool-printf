d62da5fe0e1f862379e6e8df45619fc1c1c88e62
Sebastion Price <sebastionprice@gmail.com>
2026-03-18 13:44:59 +1100

    Add full %c functionality

--- printers.c
+++ printers.c
@@ -10,10 +10,42 @@ void prntchar(char a)
 }
 
 /**
+ * prntc - print a character with formating
+ * @flager: pointer to flag type variable
+ * @c: character to print
  */
 void prntc(flag *flager, char c)
 {
+	char *a;
+	int i = 0;
+	int n;
+
 	if (flager == NULL)
 		return;
-	write(1, &c, 1);
+
+	if (flager->width)
+		n = flager->width;
+	else
+		n = 1;
+
+	a = malloc((n + 1) * sizeof(*a));
+	if (!a)
+		return;
+
+	a[n] = '\0';
+	while (i < n)
+	{
+		a[i++] = ' ';
+	}
+
+	if (flager->minus)
+		a[0] = c;
+	else
+		a[n-1] = c;
+
+	while (*a)
+	{
+		write(1, a, 1);
+		a++;
+	}
 }
