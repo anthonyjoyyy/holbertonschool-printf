8c405b2e0c6762e8766c4a6ba462181e11c7bcfd
Sebastion Price <sebastionprice@gmail.com>
2026-03-18 11:11:56 +1100

    Add comment for flag stuct

--- main.h
+++ main.h
@@ -5,6 +5,15 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/**
+ * struct flag - data type for storing flag setings
+ * @zero: 1, if true, else 0
+ * @plus: 1, if true, else 0
+ * @minus: 1, if true, else 0
+ * @space: 1, if true, else 0
+ * @width: 1, if true, else 0
+ * @per: 1, if true, else 0
+ */
 typedef struct flag
 {
 	int zero;
