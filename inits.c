4c576d863d4fbdc10088d6111edbaa37b101e976
Sebastion Price <sebastionprice@gmail.com>
2026-03-19 11:42:49 +1100

    Move finit to inits.c

--- inits.c
+++ inits.c
@@ -15,3 +15,19 @@ void finit(flag *f)
 	f->width = 0;
 	f->per = 0;
 }
+/**
+ * sinit - inialize a string to ' ', and terminate
+ * @str: pointer to string
+ * length of string not including NULL byte
+ */
+void sinit(char *str, int n)
+{
+	int i = 0;
+	
+	str[n] = '\0';
+	while(i < n)
+	{
+		str[i] = ' ';
+		i++;
+	}
+}
