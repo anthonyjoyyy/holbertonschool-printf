324ddfe7e5a24fa8dfcf9773d5a62412a13608aa
Sebastion Price <sebastionprice@gmail.com>
2026-03-21 01:34:16 +1100

    Rework write return

--- printers.c
+++ printers.c
@@ -59,10 +59,7 @@ int prnts(flag *flager, char *str)
 	if (flager == NULL)
 		return (0);
 	if (str == NULL)
-	{
-		write(1, &"(null)", 6);
-		return (6);
-	}
+		return (write(1, &"(null)", 6));
 	stln = fstrlen(flager, str);
 	lstr = lstrgen(str, stln);
 	if (flager->width > stln)
