057dfbb979506730eb356dea8659ce5806d34db0
Sebastion Price <sebastionprice@gmail.com>
2026-03-20 13:39:11 +1100

    Clean specifier.c

--- specifier.c
+++ specifier.c
@@ -2,31 +2,28 @@
 
 /**
  * specifier - call apopriate function for printing based on given char
- * @flager:
- * @
+ * @flager: flag struct storing formating options
+ * @varg: va_list varable to print
+ * @c: format character
+ * Return: number of characters printed
  */
 int specifier(flag *flager, va_list varg, char c)
 {
 	int cc;
-	
+
 	switch (c)
 	{
 	case 'c':
 		cc = prntc(flager, va_arg(varg, int));
 		return (cc);
-		break;
 	case 's':
 		cc = prnts(flager, va_arg(varg, char *));
 		return (cc);
-		break;
 	case 'd':
-		return(0);
-		break;
+		return (0);
 	case 'f':
-		return(0);
-		break;
+		return (0);
 	default:
 		return (0);
-		break;
 	}
 }
