360dc31116a2f10d78f84c72147f42c490296497
Sebastion Price <sebastionprice@gmail.com>
2026-03-23 23:23:29 +1100

    Update test list

---
+++ tests/test17.c
@@ -0,0 +1,14 @@
+#include <limits.h>
+#include "../main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_printf("testing multible paramaters:[one: %s\ttwo: %s]\n", "hello", "world");
+	printf("testing multiple parameters:[one: %s\ttwo: %s]\n", "hello", "world");
+    return (0);
+}
